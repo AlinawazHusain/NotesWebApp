61121e5897f5b93322fdfa75b55020eae7875166
Alinawaz Husain <114860703+AlinawazHusain@users.noreply.github.com>
2025-02-16 23:01:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
+const PORT = 3000;
+
+const app = express();
+
+//Middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
+app.set("view engine" , "ejs")
+app.set("views" , path.join(__dirname , 'views'));
+
+
+app.get('/' , async(req , res)=>{
+    try{
+        var data = [];
+        const rootdir = path.join(__dirname , 'notes');
+        try{
+            const files = await fs.readdir(rootdir);
+            for (const file of files){
+                const filepath = path.join(__dirname , 'notes' , file);
+                var notedata = await fs.readFile(filepath , 'utf-8');
+                const tempdata = {[file.slice(0 , -4)]: notedata};
+                data.push(tempdata);
+            }
+            res.render('home' , {data});
+        }
+        catch(err){
+            res.render('home' , {data});
+        }
+    }
+    catch(err){
+        console.error('Error reading directory or files:', err);
+        res.status(500).send("Error reading files");
+    }
+});
+
+app.post('/submit' , async(req , res)=>{
+    const title = req.body.Title;
+    const note = req.body.note;
+    const rootdir = path.join(__dirname , 'notes');
+    try{
+        const files = await fs.readdir(rootdir);
+    }
+    catch(err){
+        await fs.mkdir('notes');
+    }
+    await fs.writeFile(`notes/${title}.txt` , note , (err)=>{
+        if(err){
+            return res.status(500).send("Error in writing file");
+        }
+    });
+    res.redirect('/'); 
+});
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
